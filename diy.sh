60e71010e49567902aa790dfa095e1fe08ce10bb
“leopardciaw” <quanyongw@gmail.com>
2020-03-06 13:58:35 +0800

    update

---
+++ diy.sh
@@ -0,0 +1,53 @@
+#/bin/bash
+#=================================================
+# Description: OpenWrt DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP@sed '1,3s/my/your/g
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+#sed -i '611s/15744/32448/' target/linux/ramips/image/mt7621
+
+# 修改K2P闪存和内存
+sed -i '673s/15744/32448/g' target/linux/ramips/image/mt7621.mk
+sed -i '93s/0xf60000/0x1fb0000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+# rm -rf target/linux/ramips/image/mt7621.mk
+# rm -rf target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+# cp -r ./k2pback.d/mt7621.mk target/linux/ramips/image/
+# cp -r ./k2pback.d/mt7621_phicomm_k2p.dts target/linux/ramips/dts/
+
+# 删除原主题包
+# rm -rf package/lean/luci-theme-argon
+# rm -rf openwrt/package/lean/luci-theme-netgear
+
+# 添加新的包
+# git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc
+
+# 取消bootstrap为默认主题
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+#增加koolddns
+git clone https://github.com/xrouterservice/luci-app-koolddns.git package/lean/luci-app-koolddns
+
+# 更新
+# ./scripts/feeds update -a && ./scripts/feeds install -a
+
+##########
+# Modify the version number
+sed -i 's/OpenWrt/P3TERX build $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Add kernel build user
+[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_USER="P3TERX"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"P3TERX"@' .config
+
+# Add kernel build domain
+[ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
