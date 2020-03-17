9ff830e66823ee4ad61ba0dd4d53f4c10bfc2a93
leopardciaw <47556424+leopardciaw@users.noreply.github.com>
2020-03-17 22:52:51 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -4,14 +4,16 @@
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
+# sed '1,3s/my/your/g'
+# sed -i '93s/0xf60000/0x1fb0000/g' target/
 #=================================================
-# Modify default IP@sed '1,3s/my/your/g 
+# Modify default IP
 # sed -i 's/15744/32448/g' https://github.com/coolsnowwolf/lede/blob/3d3a95e5b440892df849a85b2dfb8482414ee43a/target/linux/ramips/image/mt7621.mk#L686
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # 修改K2P闪存和内存
-sed -i '686s/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
-sed -i '93s/0xf60000/0x1fb0000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
+sed -i 's/0xf60000/0x1fb0000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 # rm -rf target/linux/ramips/image/mt7621.mk
 # rm -rf target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 # cp -r ./k2pback.d/mt7621.mk target/linux/ramips/image/
@@ -21,7 +23,7 @@ sed -i '93s/0xf60000/0x1fb0000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 # rm -rf package/lean/luci-theme-argon
 # rm -rf openwrt/package/lean/luci-theme-netgear
 
-# 添加新的包
+# 添加新的主题包
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
 git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc
