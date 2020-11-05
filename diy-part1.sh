b7f71318865bf45236e31e633ef516745f4086eb
leopardciaw <quanyongw@gmail.com>
2020-11-05 22:53:11 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,14 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 git clone https://github.com/xiaorouji/openwrt-package/tree/master/lienol/luci-app-passwall package/luci-app-passwall
 git clone https://github.com/kenzok8/small.git package/small
 
+# 删除和替换文件，便于K2P支持32M闪存（没有硬改为32M的不用此步骤）
+# sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
+# sed -i 's/0xf60000/0x1fb0000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+rm -rf target/linux/ramips/image/mt7621.mk
+rm -rf target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+cp -rf ../mt7621.mk target/linux/ramips/image/
+cp -rf ../mt7621_phicomm_k2p.dts target/linux/ramips/dts/
+
 # 增加ssr
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 # git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
--- diy-part2.sh
+++ diy-part2.sh
@@ -19,10 +19,10 @@ sed -i 's/OpenWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
 # 删除和替换文件，便于K2P支持32M闪存（没有硬改为32M的不用此步骤）
 # sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
 # sed -i 's/0xf60000/0x1fb0000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-rm -rf target/linux/ramips/image/mt7621.mk
-rm -rf target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-cp -rf ../mt7621.mk target/linux/ramips/image/
-cp -rf ../mt7621_phicomm_k2p.dts target/linux/ramips/dts/
+# rm -rf target/linux/ramips/image/mt7621.mk
+# rm -rf target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+# cp -rf ../mt7621.mk target/linux/ramips/image/
+# cp -rf ../mt7621_phicomm_k2p.dts target/linux/ramips/dts/
 
 # 取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
